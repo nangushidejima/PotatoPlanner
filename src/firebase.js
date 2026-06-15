4184d5ec6f083c2d3d61a174e140a44ac50e2e63
nangushidejima <h6v5wdvtvp@privaterelay.appleid.com>
2026-06-15 16:19:08 +0900

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,46 @@
+import { initializeApp } from "firebase/app";
+
+import {
+getAuth
+} from "firebase/auth";
+
+import {
+getFirestore
+} from "firebase/firestore";
+
+
+// Firebase Consoleで取得した内容に
+// あとで入れ替えます
+
+const firebaseConfig = {
+
+apiKey: "YOUR_API_KEY",
+
+authDomain:
+"YOUR_AUTH_DOMAIN",
+
+projectId:
+"YOUR_PROJECT_ID",
+
+storageBucket:
+"YOUR_STORAGE_BUCKET",
+
+messagingSenderId:
+"YOUR_MESSAGING_SENDER_ID",
+
+appId:
+"YOUR_APP_ID"
+
+};
+
+
+const app =
+initializeApp(firebaseConfig);
+
+
+export const auth =
+getAuth(app);
+
+
+export const db =
+getFirestore(app);
