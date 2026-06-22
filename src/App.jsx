f9e5d1eea7b2f40a105bb0685d6f7bf5c8cd01c5
nangushidejima <h6v5wdvtvp@privaterelay.appleid.com>
2026-06-22 16:53:22 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+export default function App(){
 
-export default function App() {
 return (
-<div style={{ padding: "20px" }}>
+<div>
+
 <h1>🥔 Potato Planner</h1>
+
 <p>デジマ 生産管理</p>
+
 </div>
-);
+)
+
 }
